180a240076404ea37303efdafe852e5d81ff3de7
Lilia <sltvu93@gmail.com>
2019-12-18 17:53:04 +0300

    18.12.2019

--- src/main.js
+++ src/main.js
@@ -6,8 +6,25 @@ jQuery(function($) {
 
     console.log('YEP');
 
-    
+    var wWindow = $(window).width();
 
+    $(function() {
+      $('.header_burger').click(function(){
+        $('.header_burger').toggleClass('active');
+
+        if (wWindow < 768) {
+          $('.header_button, .header_nav, .header_address').fadeToggle(300);
+        } else {
+          $('.header_nav').fadeToggle(300);
+        }
+
+      });
+    });
+
+  });
+
+  $('ul.menu__item li:nth-child(2)').hover(function(){
+    $('ul.none').fadeToggle(300);
   });
 
 });
