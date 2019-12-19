420604aa1d6bccb0cd2db56bc3204afe5a3be0dc
Lilia <sltvu93@gmail.com>
2019-12-19 12:06:45 +0300

    меню

--- src/main.js
+++ src/main.js
@@ -21,10 +21,57 @@ jQuery(function($) {
       });
     });
 
-  });
 
-  $('ul.menu__item li:nth-child(2)').hover(function(){
-    $('ul.none').fadeToggle(300);
+
+    $(window).scroll(scrollArrow);
+
+    function scrollArrow() {
+
+      $nnn = 0;
+
+      var scrolled = $(window).scrollTop();
+
+      if (scrolled > 10) {
+
+        if (wWindow > 1200) {
+          $('.header_nav nav.menu ul.none').fadeOut(300);
+
+          $('ul.menu__item li.none').hover(function(){
+           $('ul.none').fadeToggle(300);
+
+           $nnn = $nnn + 1;
+
+           console.log($nnn);
+         });
+        }
+
+        $('.header_burger').removeClass('active');
+
+        if (wWindow < 768) {
+          $('.header_button, .header_nav, .header_address').fadeOut(300);
+        } else {
+
+          if (wWindow < 1200) {
+            $('.header_nav').fadeOut(300);
+          }
+        }
+      }
+    }
+
+    $('.yes input').on('click', function () {
+
+      console.log('YEP');
+
+      $('.yes').toggleClass('not-active');
+    });
+
+
+
+
+
+
+
+
   });
 
 });
